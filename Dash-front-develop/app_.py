d5cfe76f15cddcfb78389944351d14c32fbbd150
d-ocampo <daocampol@unal.edu.co>
2021-03-17 21:26:11 -0500

    Pagina final

--- Dash-front-develop/app_.py
+++ Dash-front-develop/app_.py
@@ -3,7 +3,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__,
-                external_stylesheets=[dbc.themes.BOOTSTRAP],
+                external_stylesheets=["./assets/boostrap.min.css"],
                 # these meta_tags ensure content is scaled correctly on different devices
                 # see: https://www.w3schools.com/css/css_rwd_viewport.asp for more
                 meta_tags=[
